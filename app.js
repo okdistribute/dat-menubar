22e51139bb3a588d863a5876d2ea59aec7cd8b8e
Karissa McKelvey <krmckelv@gmail.com>
2015-12-11 18:01:12 -0800

    dat js integration

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var menubar = require('menubar')
 var ipc = require('electron').ipcMain
+var fs = require('fs')
+var mkdirp = require('mkdirp')
+var dat = require('dat')
 var path = require('path')
+var dialog = require('electron').dialog
+var homedir = require('os-homedir')()
+
+var datPath = path.join(homedir, '.dat')
+mkdirp.sync(datPath)
+var configFile = path.join(datPath, 'config.json')
+var RUNNING = {}
 
 var mb = menubar({
   dir: __dirname,
@@ -12,10 +22,137 @@ mb.on('ready', function ready () {
   console.log('ready')
 })
 
+process.on('uncaughtException', function (err) {
+  dialog.showErrorBox('Uncaught Exception: ' + err.message, err.stack || '')
+  mb.app.quit()
+})
+
+mb.on('show', function show () {
+  app.configure(mb.window.webContents)
+  app.send('show')
+})
+
 ipc.on('terminate', function terminate (ev) {
   mb.app.terminate()
 })
 
 ipc.on('hide', function hide (ev) {
   mb.hideWindow()
+})
+
+var Server = require('electron-rpc/server')
+var app = new Server()
+
+function writeConfig (data) {
+  if (typeof data === 'object') data = JSON.stringify(data, null, 2)
+  fs.writeFileSync(configFile, data)
+}
+
+function loadConfig () {
+  var conf, data
+  try {
+    data = fs.readFileSync(configFile)
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      var defaultConfig = fs.readFileSync(path.join(__dirname, 'config.json')).toString()
+      writeConfig(defaultConfig)
+      return loadConfig()
+    } else {
+      throw e
+    }
+  }
+
+  try {
+    conf = JSON.parse(data.toString())
+  } catch (e) {
+    var code = dialog.showMessageBox({
+      message: 'Invalid configuration file\nCould not parse JSON',
+      detail: e.stack,
+      buttons: ['Reload Config', 'Exit app']
+    })
+    if (code === 0) {
+      return loadConfig()
+    } else {
+      mb.app.quit()
+      return
+    }
+  }
+
+  return conf
+}
+
+
+mb.on('ready', function ready () {
+  configure()
+
+  app.on('dats', function (req, cb) {
+    var conf = loadConfig()
+    cb(conf.dats)
+  })
+
+  app.on('get-one', function (req, cb) {
+    var conf = loadConfig()
+    cb(conf.dats[req.body.path])
+  })
+
+  app.on('start', function task (req, cb) {
+    start(req.body, cb)
+  })
+
+  app.on('stop', function task (req, cb) {
+    stop(req.body, cb)
+  })
+
+  function start (data, cb) {
+    var config = loadConfig()
+    var db = dat(data.dat.path)
+    if (RUNNING[data.dat.path]) return cb(null, data.dat)
+    if (data.dat.link) db.serve(data.dat.link, done)
+    else db.share(done)
+
+    function done (err, link, port, close) {
+      if (err) return cb(err)
+      RUNNING[data.dat.path] = close
+
+      data.dat.link = link
+      data.dat.active = true
+      data.dat.date = Date.now()
+      config.dats[data.dat.path] = data.dat
+      writeConfig(config)
+
+      cb(null, data.dat)
+    }
+  }
+
+  function stop (data, cb) {
+    var config = loadConfig()
+    var close = RUNNING[data.dat.path]
+    if (close) close(done)
+    else done()
+    function done (err) {
+      if (err) return cb(err)
+      console.log('closing.')
+      RUNNING[data.dat.path] = undefined
+
+      data.dat.active = false
+      config.dats[data.dat.path] = data.dat
+      writeConfig(config)
+
+      cb(null, data.dat)
+    }
+  }
+
+  function configure () {
+    var conf = loadConfig()
+    var keys = Object.keys(conf.dats)
+    for (var i = 0; i < keys.length; i++) {
+      var dat = conf.dats[keys[i]]
+      if (dat.active) {
+        start({dat: dat}, function (err, dat) {
+          if (err) throw err
+          console.log('Started', dat.link)
+        })
+      }
+    }
+  }
 })
--- index.js
+++ index.js
@@ -10,99 +10,104 @@ var dialog = electron.remote.dialog
 var Menu = electron.remote.Menu
 var MenuItem = electron.remote.MenuItem
 
+var Client = require('electron-rpc/client')
+var client = new Client()
+client.request('dats', function (dats) {
+  console.log(dats)
+  render(dats)
+})
+
 var IMG_PATH = path.join(__dirname, 'static', 'images')
 
-var dats = [
-  {
-    name: 'karissa/cfpb',
-    path: '/Users/karissa/dev/dats/cfpb',
-    date: Date.now(),
-    active: true
-  },
-  {
-    name: 'sunlight/elections',
-    path: '/Users/karissa/Dropbox/dats/elections',
-    date: new Date(123423422342),
-    active: true
-  }
-]
+function render (dats) {
+  Ractive({
+    el: '#container',
+    template: fs.readFileSync(path.join(__dirname, './templates/list.html')).toString(),
+    data: {dats: dats, IMG_PATH: IMG_PATH},
+    onrender: function () {
+      var self = this
 
-Ractive({
-  el: '#container',
-  template: fs.readFileSync(path.join(__dirname, './templates/list.html')).toString(),
-  data: {dats: dats, IMG_PATH: IMG_PATH},
-  onrender: function () {
-    var self = this
-
-    dragDrop(document.querySelector('#content'), function (files) {
-      var file = files[0]
-      dats.push(Dat({path: file.path}))
-    })
-
-    self.on('toggle', function (event, i) {
-      dats[i].active = !dats[i].active
-      self.set('dats', dats)
-      event.original.preventDefault()
-      event.original.stopPropagation()
-    })
-
-    self.on('share', function (event, i) {
-      var dat = self.get('dats')[i]
-      event.original.preventDefault()
-      event.original.stopPropagation()
-    })
-
-    self.on('open', function (event, i) {
-      var dat = dats[i]
-      shell.openItem(dat.path)
-      ipc.send('hide')
-    })
-
-    self.on('info', function (event, i) {
-      var dat = dats[i]
-      console.log(dat)
-    })
-
-    var contextMenu = new Menu()
-    contextMenu.append(new MenuItem({ label: 'Copy link', click: function () { self.fire('share') } }))
-    contextMenu.append(new MenuItem({ label: 'Copy path', click: function () { self.fire('share') } }))
-    contextMenu.append(new MenuItem({ label: 'Publish new version', click: function () { self.fire('publish') } }))
-
-    var rows = document.getElementsByClassName('row')
-    for (var i = 0; i < rows.length; i++) {
-      var item = rows[i]
-      item.addEventListener('contextmenu', function (e) {
-        e.preventDefault()
-        contextMenu.popup(electron.remote.getCurrentWindow())
+      dragDrop(document.querySelector('#content'), function (files) {
+        var file = files[0]
+        dats.push(Dat({path: file.path}))
       })
-    }
-  }
-})
 
-Ractive({
-  el: '#footer',
-  template: fs.readFileSync(path.join(__dirname, './templates/footer.html')).toString(),
-  data: {IMG_PATH: IMG_PATH},
-  onrender: function () {
-    var self = this
-    var settings = new Menu()
-    settings.append(new MenuItem({ label: 'Debug' }))
-    settings.append(new MenuItem({ label: 'Stop sharing and quit', click: function () { ipc.send('terminate') } }))
-    self.on('settings', function (event) {
-      event.original.preventDefault()
-      settings.popup(electron.remote.getCurrentWindow())
-    })
-    self.on('add', function (event) {
-      var opts = { properties: [ 'openFile', 'openDirectory' ] }
-      dialog.showOpenDialog(opts, function (files) {
-        if (!files) return
-        files.map(function (file) {
-          dats.push(Dat({path: file}))
+      self.on('stop', function (event, i) {
+        client.request('stop', {dat: dats[i]}, function (err, dat) {
+          if (err) throw err
+          dats[i] = dat
+          console.log(dat)
+          self.set('dats', dats)
+        })
+        event.original.preventDefault()
+        event.original.stopPropagation()
+      })
+
+      self.on('share', function (event, i) {
+        client.request('start', {dat: dats[i]}, function (err, dat) {
+          if (err) return console.error(err)
+          dats[i] = dat
+          self.set('dats', dats)
+          electron.clipboard.writeText(dat.link)
+        })
+        event.original.preventDefault()
+        event.original.stopPropagation()
+      })
+
+      self.on('open', function (event, i) {
+        var dat = dats[i]
+        shell.openItem(dat.path)
+        ipc.send('hide')
+      })
+
+      self.on('info', function (event, i) {
+        var dat = dats[i]
+        console.log(dat)
+      })
+
+      var contextMenu = new Menu()
+      contextMenu.append(new MenuItem({ label: 'Copy link', click: function () { self.fire('share') } }))
+      contextMenu.append(new MenuItem({ label: 'Publish new version', click: function () { self.fire('publish') } }))
+
+      var rows = document.getElementsByClassName('row')
+      for (var i = 0; i < rows.length; i++) {
+        var item = rows[i]
+        item.addEventListener('contextmenu', function (e) {
+          e.preventDefault()
+          contextMenu.popup(electron.remote.getCurrentWindow())
+        })
+      }
+    }
+  })
+
+  Ractive({
+    el: '#footer',
+    template: fs.readFileSync(path.join(__dirname, './templates/footer.html')).toString(),
+    data: {IMG_PATH: IMG_PATH},
+    onrender: function () {
+      var self = this
+      var settings = new Menu()
+      settings.append(new MenuItem({ label: 'Debug' }))
+      settings.append(new MenuItem({ label: 'Stop sharing and quit', click: function () { ipc.send('terminate') } }))
+      self.on('settings', function (event) {
+        event.original.preventDefault()
+        settings.popup(electron.remote.getCurrentWindow())
+      })
+      self.on('add', function (event) {
+        var opts = { properties: [ 'openFile', 'openDirectory' ] }
+        dialog.showOpenDialog(opts, function (files) {
+          if (!files) return
+          files.map(function (file) {
+            var dat = Dat({path: file})
+            dats[dat.path] = dat
+            self.set('dats', dats)
+            self.fire('share', dat.path)
+          })
         })
       })
-    })
-  }
-})
+    }
+  })
+}
 
 function throwError (error) {
   var message = error.stack || error.message || JSON.stringify(error)
